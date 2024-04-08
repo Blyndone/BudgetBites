e87c508be85eb83e5b53b1d9bdc996e20c7915a0
Rick Duel <81060949+Blyndone@users.noreply.github.com>
2024-04-08 08:21:14 -0400

    Flex Server

--- server.js
+++ server.js
@@ -18,22 +18,29 @@ const mysql = require('mysql2');
 // connecting Database
 
 //===========================================
-const connection = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  multipleStatements: true,
-});
-// const connection = mysql.createPool({
-//   host: process.env.HOST,
-//   user: process.env.USER,
-//   password: process.env.PASSWORD,
-//   database: process.env.DATABASE,
-//   port: process.env.PORT,
-//   multipleStatements: true,
-// });
-// const saltRounds = 10;
+
+var con = {};
+if (process.env.PORT == '5000') {
+  con = {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    multipleStatements: true,
+  };
+} else {
+  con = {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT,
+    multipleStatements: true,
+  };
+}
+
+const connection = mysql.createPool(con);
+const saltRounds = 10;
 
 //===========================================
 
